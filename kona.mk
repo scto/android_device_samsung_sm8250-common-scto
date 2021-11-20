9bff71d9d0366509dc8b1ea78051a75f8d962103
scto <tschmid35@gmail.com>
2021-11-20 20:12:00 +0100

    sm8250-common: Build QTI USB HIDL

--- kona.mk
+++ kona.mk
@@ -209,5 +209,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.2-service-qti
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/samsung/sm8250-common/sm8250-common-vendor.mk)
